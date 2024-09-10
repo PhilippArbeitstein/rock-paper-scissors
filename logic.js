099af0a98a2aa8f6a157f45cde453c926c921f9d
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-10 10:56:12 +0200

    Random computer choice implemented

--- logic.js
+++ logic.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+function getComputerChoice() {
+    let rdm = Math.random();
+    if(rdm <= 1/3) {
+        return "rock";
+    } else if(rdm > 1/3 && rdm <= 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+console.log(getComputerChoice());
